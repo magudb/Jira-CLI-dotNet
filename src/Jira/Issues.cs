322e361bc6bc7d6be3253313b0af716f2ff9ac5a
magnus udbjorg <magnus@udbjorg.net>
2017-03-13 22:25:02 +0100

    updates

--- src/Jira/Issues.cs
+++ src/Jira/Issues.cs
@@ -4,7 +4,6 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Jira
 {
--- src/Jira/Program.cs
+++ src/Jira/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.IO;
-using Microsoft.Extensions.CommandLineUtils;
-using Newtonsoft.Json;
+﻿using Microsoft.Extensions.CommandLineUtils;
 
 namespace Jira
 {
